9953452eef62852971e188d5a32f5538356b526d
Jessy <jessysanfilippo02@outlook.fr>
2025-09-25 14:45:41 +0200

    new branch new me

--- python/main.py
+++ python/main.py
@@ -11,7 +11,7 @@ from scipy.linalg import solve_discrete_are
 
 
 # Run if you want plots in external windows
-# %matplotlib
+%matplotlib qt
 
 
 # Run the ipython magic below to activate automated import of modules. Useful if you write code in external .py files.
@@ -103,8 +103,7 @@ class PurePursuitController(PurePursuitControllerBase):
         self.l = l
         self.L = L
         self.goal_tol = goal_tol
-        self.idx_start = 0
-        self.last_idx = 0
+        self.last_index = 0
 
     def pursuit_point(self, p_car):
         """Return pure-pursuit given the position of the car.
@@ -119,22 +118,22 @@ class PurePursuitController(PurePursuitControllerBase):
 
         path_points = self.plan.path  # Points on the path
         l = self.l  # Pure-pursuit look-ahead
+        # Your code here
+        for i in range(self.last_index, len(path_points)):
+            if abs(np.linalg.norm(path_points[i] - p_car)) >= l:
+                p_purepursuit = path_points[i]
+                print("Pursuit point index:", p_purepursuit)
+                self.last_index = i
+                
+                return p_purepursuit
 
-        # Exercise 5.1
-
+        return path_points[-1]
+                
         # Hint: It is typically not important to find a point at _exactly_ distance l,
         #       for example search pure-pursuit point among the points in path_points
         #       but don't forget to take into account the approximate pursuit-horizon when
         #       computing the steering angle.
-        
-        self.idx_start = self.last_idx
-        for i in range(self.idx_start, len(path_points)):
-            dist = np.linalg.norm(path_points[i] - p_car)
-            if dist >= l:
-                self.last_idx = i
-                return path_points[i]
-        return path_points[-1] # if no point found, return goal
-
+        return p_purepursuit
 
     def pure_pursuit_control(self, dp, theta):
         """Compute pure-pursuit steer angle.
@@ -148,15 +147,14 @@ class PurePursuitController(PurePursuitControllerBase):
         """
 
         # Your code here to compute new steering angle
-        L = self.L
-        x = dp[0]
-        y = dp[1]
-        ld2 = x**2 + y**2  # squared lookahead distance
+        dx, dy = dp
+        x_local = np.cos(theta) * dx + np.sin(theta) * dy
+        y_local = -np.sin(theta) * dx + np.cos(theta) * dy
 
-        if ld2 == 0:
-            return 0.0  # Avoid division by zero
+        ld2 = x_local**2 + y_local**2
 
-        delta = np.arctan2(2 * L * y, ld2)
+        tandelta = 2 * self.L * y_local / ld2
+        delta = np.arctan(tandelta)
         return delta
 
     def u(self, t, w):
@@ -175,22 +173,10 @@ class PurePursuitController(PurePursuitControllerBase):
         # Your code here to compute steering angle, use the functions
         # self.pursuit_point() and self.pure_pursuit_control() you
         # have written above.
-        # 1. Find pursuit point
         p_purepursuit = self.pursuit_point(p_car)
-
-        # 2. Vector to pursuit point (global frame)
-        dp_global = p_purepursuit - p_car
-
-        # 3. Transform to car frame
-        R = np.array([
-            [np.cos(-theta), -np.sin(-theta)],
-            [np.sin(-theta),  np.cos(-theta)]
-        ])
-        dp_car = R @ dp_global
-
-        # 4. Compute steering angle
-        delta = self.pure_pursuit_control(dp_car, theta)
-        acc = 0 
+        dp = p_purepursuit - p_car
+        delta = self.pure_pursuit_control(dp, theta)
+        acc = 0  # Constant speed
 
         self._pursuit_plot(p_car, p_purepursuit)  # Included for animation purposes.
 
@@ -206,14 +192,13 @@ class PurePursuitController(PurePursuitControllerBase):
         return dist > self.goal_tol**2
 
 
-
 # %%# Assertions
-
+# test = PurePursuitController(l=4, L=2, path=nom_path, goal_tol=0.25)
+# test.pursuit_point(np.array([0, 0]))
 # A few tests on your implementation. Note that passing these tests doesn't imply that your solution is correct but do not submit a solution if your solution doesn't pass these tests. First, test the ```pure_pursuit_control``` function
 
 pp_controller = PurePursuitController(l=4, L=car.L, path=nom_path, goal_tol=0.25)
-
-print ("value : ", abs(pp_controller.pure_pursuit_control(np.array([1.0, 1.0]), 10 * np.pi / 180) - 1.01840))
+print(abs(pp_controller.pure_pursuit_control(np.array([1.0, 1.0]), 10 * np.pi / 180) - 1.01840))
 
 assert (
     abs(pp_controller.pure_pursuit_control(np.array([1.0, 1.0]), 10 * np.pi / 180) - 1.01840) < 1e-3
@@ -311,5 +296,3 @@ class StateFeedbackController(ControllerBase):
 
 # %%
 plt.show()
-
-# %%
